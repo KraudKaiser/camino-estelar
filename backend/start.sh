4ebadbcdded3dc286ce15053e029ecd43f9cabea
UAMI-PC-Ramiro <ramiro@uami.ai>
2026-07-08 16:16:12 -0300

    start db instructions fixed

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Running database migrations..."
-npx prisma migrate deploy
+echo "Pushing database schema..."
+npx prisma db push --accept-data-loss
 
 echo "Seeding database..."
 npx tsx src/prisma/seed.ts
